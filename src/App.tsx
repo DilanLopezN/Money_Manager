b8da307591d531524582fe278ba25b529b5b04cb
Dilan Lopez <dilanlopez009@gmail.com>
2022-11-10 16:26:30 -0300

    first styled component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import styled from 'styled-components'
+const Title = styled.h1`
+  color: #8257e6;
+`
+
 export function App() {
   return (
     <div className="App">
-      <h1>Hellow World</h1>
+      <Title>Hellow World</Title>
     </div>
   )
 }
